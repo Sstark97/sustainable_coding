6b7d595997ae2e0111ebba7f46d041c7a0d93daa
Sstark97 <aitor@leanmind.es>
2023-11-20 20:13:27 +0000

    Test(:white_check_mark:): TemplateEngineShould parse a template with one variable -> Add another case

--- src/main/kotlin/dev/kata/stringcalculator/TemplateEngine.kt
+++ src/main/kotlin/dev/kata/stringcalculator/TemplateEngine.kt
@@ -5,7 +5,8 @@ class TemplateEngine {
         // private var logs: List<String> = listOf("La variable var1 no se ha podido sustituir porque no fue encontrada")
 
         fun parse(template: String, templateVariables: Map<String, String>): String {
-            return template.replace("{\$placeholder}", "mundo")
+            val variableValue = templateVariables.get("placeholder") ?: ""
+            return template.replace("{\$placeholder}", variableValue)
         }
 
 //        fun showLogs(): List<String> {
