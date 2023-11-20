fe223fd62919316c9a7abfd57507bf4f535f5308
Sstark97 <aitor@leanmind.es>
2023-11-20 21:39:07 +0000

    Test(:white_check_mark:) TemplateEngineShould parse all occurrences in a template with one variable

--- src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
+++ src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
@@ -3,9 +3,10 @@ package dev.kata.template_engine
 class TemplateEngine {
     companion object {
         fun parse(template: String, templateVariables: Map<String, String>): Template {
-            val variableValue = templateVariables.values.toTypedArray().firstOrNull() ?: ""
-            val keyToReplace = templateVariables.keys.toTypedArray().firstOrNull() ?: ""
-            val parsedTemplate = template.replace("{\$" + keyToReplace + "}", variableValue)
+            var parsedTemplate = template
+            templateVariables.forEach { (key, value) ->
+                parsedTemplate = parsedTemplate.replace("{\$" + key + "}", value)
+            }
             return Template(parsedTemplate)
         }
     }
