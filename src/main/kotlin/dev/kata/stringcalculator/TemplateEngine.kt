7f175bf0d38c5fcd0157beeaf2cf553fc7d254e6
Sstark97 <aitor@leanmind.es>
2023-11-20 14:22:44 +0000

    Test(:white_check_mark:): TemplateEngineShould parse a simple template without variables

--- src/main/kotlin/dev/kata/stringcalculator/TemplateEngine.kt
+++ src/main/kotlin/dev/kata/stringcalculator/TemplateEngine.kt
@@ -3,8 +3,7 @@ package dev.kata.stringcalculator
 class TemplateEngine {
     companion object {
         fun parse(template: String, templateVariables: Map<String, String>): String {
-            TODO("Not yet implemented")
+            return ""
         }
     }
-
 }
