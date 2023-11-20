bbff3f853cdd6822d6dcf8de39f07bd8a261bf4e
Sstark97 <aitor@leanmind.es>
2023-11-20 22:55:37 +0000

    :recycle: Change to a version with foreach and extract method to findAllVariables

--- src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
+++ src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
@@ -3,21 +3,28 @@ package dev.kata.template_engine
 class TemplateEngine {
     companion object {
         fun parse(template: String, templateVariables: Map<String, String>): Template {
+            var parsedTemplate = template
             val warnings = mutableListOf<String>()
-            val variableRegex = Regex("\\{\\$\\w+\\}")
-            val parsedTemplate = template.replace(variableRegex) { match ->
-                val matchValue = match.value
-                val key = matchValue.substring(2, matchValue.length - 1)
+            val variables = findAllVariablesIn(template)
+
+            variables.forEach { match ->
+                val key = match.substring(2, match.length - 1)
                 val variableValue = templateVariables.get(key)
 
                 if (variableValue.isNullOrBlank()) {
                     warnings.add("The variable $key could not be replaced because it was not found")
-                    matchValue
                 } else {
-                    variableValue
+                    parsedTemplate = parsedTemplate.replace("{\$" + key + "}", variableValue)
                 }
             }
+
             return Template(parsedTemplate, warnings)
+        }
+
+        private fun findAllVariablesIn(template: String): List<String> {
+            val variableRegex = Regex("\\{\\$\\w+\\}")
+            val variables = variableRegex.findAll(template).map { it.value }.toList()
+            return variables
         }
     }
 }
