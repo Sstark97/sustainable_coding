8c66c1ca3b5d8ff5084bcc1560618baa8e1759fe
Sstark97 <aitor@leanmind.es>
2023-11-20 20:34:36 +0000

    :recycle: Add Template class

--- src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
+++ src/main/kotlin/dev/kata/template_engine/TemplateEngine.kt
@@ -1,13 +1,14 @@
-package dev.kata.stringcalculator
+package dev.kata.template_engine
 
 class TemplateEngine {
     companion object {
         // private var logs: List<String> = listOf("La variable var1 no se ha podido sustituir porque no fue encontrada")
 
-        fun parse(template: String, templateVariables: Map<String, String>): String {
+        fun parse(template: String, templateVariables: Map<String, String>): Template {
             val variableValue = templateVariables.values.toTypedArray().firstOrNull() ?: ""
             val keyToReplace = templateVariables.keys.toTypedArray().firstOrNull() ?: ""
-            return template.replace("{\$" + keyToReplace + "}", variableValue)
+            val parsedTemplate = template.replace("{\$" + keyToReplace + "}", variableValue)
+            return Template(parsedTemplate)
         }
 
 //        fun showLogs(): List<String> {
